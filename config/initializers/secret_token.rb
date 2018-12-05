bc3bae3dc45bddee50e0fbaadf513121538c2483
Dave JC <davejc99@gmail.com>
2018-12-05 16:05:22 +0530

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DivyanInfo::Application.config.secret_token = 'd34eed77b25b01c7b8de69a588feaaff4ec62913e892ec81b7be4d524248dd4b0302d88d08195fd5053523d754c31dcf2ce54b061d5b1a1d847b83fc02b6202a'
